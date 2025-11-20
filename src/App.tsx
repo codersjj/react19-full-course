a918a12b679ab99e62aa3b9387976a007b3ca8a3
codersjj <shanewestlife@outlook.com>
2025-11-20 17:41:49 +0800

    feat: add styles

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,25 @@ const items = [
 
 interface ButtonProps {
   children?: React.ReactNode;
+  className: string;
 }
 
 const Button = (props: ButtonProps) => {
-  const { children } = props;
-  return <button>{children}</button>;
+  const { children, className } = props;
+  return <button className={className}>{children}</button>;
 };
 
 function App() {
   return (
     <div>
+      <button className="btn">Styled btn</button>
+
       {items.map((item) => {
-        return <Button key={item.id}>{item.label}</Button>;
+        return (
+          <Button key={item.id} className="btn">
+            {item.label}
+          </Button>
+        );
       })}
     </div>
   );
