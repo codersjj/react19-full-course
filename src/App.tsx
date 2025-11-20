dcb8b4a4e434932be31556842e93b5b2cdecda88
codersjj <shanewestlife@outlook.com>
2025-11-20 15:30:12 +0800

    feat: add props

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-const Button = () => {
-  return <button>Click me</button>;
+interface ButtonProps {
+  title: string;
+  text?: string;
+}
+
+const Button = (props: ButtonProps) => {
+  const { text = "button" } = props;
+  return <button>{text}</button>;
 };
 
 const Input = () => {
@@ -9,10 +15,12 @@ const Input = () => {
 function App() {
   return (
     <div>
-      {/* <button>Click me</button> */}
-      <Button />
-      <Button />
-      <Button />
+      {/* HTML button with a built-in attribute */}
+      <button title="Click me">Click me</button>
+      {/* React component with a custom attribute (prop) */}
+      <Button title="Click me" />
+      <Button title="Hi" text="Hi" />
+      <Button title="Hello" text="Say hello" />
       <Input />
     </div>
   );
