4b5fe016cc64674e0a379987c22ce0a45ea8b962
codersjj <shanewestlife@outlook.com>
2025-11-20 15:21:15 +0800

    feat: add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+const Button = () => {
+  return <button>Click me</button>;
+};
+
+const Input = () => {
+  return <input placeholder="enter" />;
+};
+
 function App() {
   return (
     <div>
-      <button>Click me</button>
+      {/* <button>Click me</button> */}
+      <Button />
+      <Button />
+      <Button />
+      <Input />
     </div>
   );
 }
