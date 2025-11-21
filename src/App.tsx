d3566af708e7224f52ac6cc834e0592582bd9473
codersjj <shanewestlife@outlook.com>
2025-11-21 16:39:28 +0800

    feat: add an example of useRef and forwardRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,14 @@
-import { useEffect, useRef } from "react";
+import { forwardRef, useEffect, useRef, type InputHTMLAttributes } from "react";
+
+// old approach, In React 19, forwardRef is no longer necessary. Pass ref as a prop instead.
+const Input = forwardRef<
+  HTMLInputElement,
+  InputHTMLAttributes<HTMLInputElement>
+>((props, ref) => {
+  return (
+    <input ref={ref} type="text" placeholder="type something..." {...props} />
+  );
+});
 
 function App() {
   const inputRef = useRef<HTMLInputElement>(null);
@@ -14,7 +24,7 @@ function App() {
   return (
     <div className="p-5">
       <h1>Auto Focus</h1>
-      <input ref={inputRef} type="text" placeholder="type something..." />
+      <Input ref={inputRef} />
     </div>
   );
 }
