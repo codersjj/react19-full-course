945d7e448013ab2be3aabfa2731363d68add7eff
codersjj <shanewestlife@outlook.com>
2025-11-22 16:37:30 +0800

    feat: add React Router example with params and query

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,5 +1,17 @@
+import { useParams, useSearchParams } from "react-router";
+
 const Post = () => {
-  return <div>Post</div>;
+  const { id } = useParams();
+  const [searchParams] = useSearchParams();
+
+  const query = searchParams.get("q");
+
+  return (
+    <div>
+      <p>Post ID: {id}</p>
+      <p>Post query: {query}</p>
+    </div>
+  );
 };
 
 export default Post;
