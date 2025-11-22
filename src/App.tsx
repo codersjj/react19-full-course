4539ab68d4d22974e0408c7bea2cfeaadbe63db3
codersjj <shanewestlife@outlook.com>
2025-11-22 11:30:16 +0800

    feat: add Redux example

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
 import Button from "./components/Button";
-import { ThemeProvider } from "./context/theme-provider";
-import { useTheme } from "./hooks/useTheme";
+import { createStore } from "redux";
+import { Provider, useDispatch, useSelector } from "react-redux";
+
+// Action
+const increment = () => ({
+  type: "INCREMENT",
+});
+
+const decrement = () => ({
+  type: "DECREMENT",
+});
+
+type CounterAction = {
+  type: string;
+};
+
+type CounterState = number;
+
+// Reducer
+const initialState: CounterState = 0;
+const countReducer = (state = initialState, action: CounterAction) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(countReducer);
 
 function App() {
   return (
-    <ThemeProvider>
+    <Provider store={store}>
       <div className="p-5">
-        <Text />
+        <Counter />
       </div>
-    </ThemeProvider>
+    </Provider>
   );
 }
 
-function Text() {
-  const { theme, toggleTheme } = useTheme();
+const Counter = () => {
+  const count = useSelector((state: CounterState) => state);
+  const dispatch = useDispatch();
 
   return (
     <>
-      <p>{theme}</p>
-      <Button onClick={toggleTheme}>Switch theme</Button>
+      <p>Count: {count}</p>
+      <Button
+        variant="outline"
+        className="cursor-pointer"
+        onClick={() => dispatch(increment())}
+      >
+        Increment
+      </Button>
+      <Button
+        variant="outline"
+        className="cursor-pointer"
+        onClick={() => dispatch(decrement())}
+      >
+        Decrement
+      </Button>
     </>
   );
-}
+};
 
 export default App;
