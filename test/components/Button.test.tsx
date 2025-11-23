39a371f8d4861837572891a52d0551ab85bf2411
codersjj <shanewestlife@outlook.com>
2025-11-23 15:30:00 +0800

    test: add getByRole query examples

--- test/components/Button.test.tsx
+++ test/components/Button.test.tsx
@@ -9,4 +9,15 @@ describe("Button Component", () => {
 
     expect(btn).toBeInTheDocument();
   });
+
+  it("should render a Button component", async () => {
+    render(<Button>Click me</Button>);
+
+    const btn = screen.getByRole("button", {
+      // name: "Click me",
+      name: /click me/i,
+    });
+
+    expect(btn).toBeInTheDocument();
+  });
 });
